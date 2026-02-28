652d46b66b70a2dd6051a24dd08ef108fcbe2d80
noteacer <rafaelangelao@gmail.com>
2026-02-27 23:06:29 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,44 @@
+﻿using MauiAppMinhasCompras.Models;
+using SQLite;
+
+namespace MauiAppMinhasCompras.Resources.Helpers
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public Task<int> Update(Produto p)
+        {
+            return _conn.UpdateAsync(p);
+        }
+
+        public Task<int> Delete(int id)
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Search(string q)
+        {
+            // Usando LINQ para evitar erros de sintaxe SQL e ataques de SQL Injection
+            return _conn.Table<Produto>()
+                        .Where(i => i.Descricao.Contains(q))
+                        .ToListAsync();
+        }
+    }
+}
---
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+
+
+
+
+
+namespace MauiAppMinhasCompras.Models
+{
+    public class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+     
+        public string Descricao { get; set; } = string.Empty;
+
+        public double Quantidade { get; set; }
+
+        public double Preco { get; set; }
+    }
+}
